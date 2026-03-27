b1eab9fbf0abef751abd86b25ee30f5d96e161b7
Biswajeet Pradhan <biswajeetpradhan36933@gmail.com>
2026-03-27 23:02:24 +0000

    27-03-2026

--- Learning Web Scrapping/2_0 web scrapping using selenium.py
+++ Learning Web Scrapping/2_0 web scrapping using selenium.py
@@ -4,21 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Setup ChromeOptions
 options = Options()
-options.add_experimental_option("detach", True) # Keeps browser open
+options.add_experimental_option("detach", True) 
 
-# --- ADDING THESE LINES TO BYPASS BOT DETECTION ---
-# if you dont put the lines below then the browser will detect that the browser is being controlled by automated software and will not allow you to access the website and will show you a captcha to solve
-
-# Removes the "Chrome is being controlled by automated software" banner
+# -----------------------------------------------
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 
-# Disables the flag that tells websites you are using an automated webdriver
 options.add_argument("--disable-blink-features=AutomationControlled")
 
-# (Optional) Set a realistic User-Agent so Google thinks you are a normal user
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 # -----------------------------------------------
 
@@ -29,14 +23,18 @@ options.binary_location = brave_path
 
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://www.python.org")
-assert 'Python' in driver.title
+search = "i phones"
+driver.get(f"https://www.amazon.in/s?k={search}&ref=nb_sb_noss")
+
+elements = driver.find_elements(By.CLASS_NAME, "puisg-row")
+for element in elements:
+    print(element.text)         # will print all the elements inside the designated class
+
+# print(element.get_attribute("outerHTML"))       # will give entire html of the mentioned class
+
+# innerHTML gives you everything inside the element (but excludes the element itself). outerHTML gives you everything inside the element, plus the element itself.
 
 
-elem = driver.find_element(By.NAME, 'q')
-elem.clear()                # By running elem.clear() first, it acts like pressing Backspace repeatedly until the box is completely empty.
-elem.send_keys('jsighseoigjhoahs', Keys.ENTER)
-assert 'NO RESULTS FOUND' not in driver.page_source
+driver.close()
 
-# time.sleep(5)
-# driver.close()
+
